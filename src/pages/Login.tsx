bb68b143d33ad5ce53f5a52852f21fd413b15dab
Yeasir Arafat <ysarafat.bd@gmail.com>
2024-02-10 00:46:51 +0600

    set cookie and decode user data

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,56 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { setUser } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/hook";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
-  return <div>login</div>;
+  const dispatch = useAppDispatch();
+  const [login, { isLoading, isSuccess, error }] = useLoginMutation();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "123456",
+    },
+  });
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(
+      setUser({
+        user,
+        token: res.data.accessToken,
+      })
+    );
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input
+          type="text"
+          id="id"
+          {...register("userId", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          {...register("password", { required: true })}
+        />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
 import authReducer from "./features/auth/authSlice";
 const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 export default store;
